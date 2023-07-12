17e6c9990a5cb31ad8f7de7adea01cdfc4995002
Wesley Romao <wesleyredx@gmail.com>
2023-07-12 13:48:00 -0300

    pacotes vuex/router/validate/axios instalados

--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
 import '@/assets/css/tailwind.css';
+import router from './router';
+import store from './store';
+import '@/plugins/vee-validate';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
